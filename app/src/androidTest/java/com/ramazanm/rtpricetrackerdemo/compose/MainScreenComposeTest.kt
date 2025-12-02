a4c776aae806603edc43904b3c0eea31843f51b6
Ramazan Mutlu <r.ramazanmutlu@gmail.com>
2025-12-02 15:00:03 +0300

    MainScreenComposeTest updated with new StockUi fields and navigation controller

--- app/src/androidTest/java/com/ramazanm/rtpricetrackerdemo/compose/MainScreenComposeTest.kt
+++ app/src/androidTest/java/com/ramazanm/rtpricetrackerdemo/compose/MainScreenComposeTest.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onChildAt
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import com.ramazanm.rtpricetrackerdemo.data.model.StockIndicator
 import com.ramazanm.rtpricetrackerdemo.data.model.StockUi
 import com.ramazanm.rtpricetrackerdemo.presentation.StockViewModel
 import com.ramazanm.rtpricetrackerdemo.ui.theme.screen.MainScreen
@@ -35,14 +37,20 @@ class MainScreenComposeTest {
                 StockUi(
                     name = "AAPL",
                     price = 300.0,
+                    indicator = StockIndicator.NEUTRAL,
+                    details = "Apple Inc."
                 ),
                 StockUi(
                     name = "MSFT",
                     price = 200.0,
+                    indicator = StockIndicator.DOWN,
+                    details = "Microsoft Corporation"
                 ),
                 StockUi(
                     name = "AMZN",
                     price = 100.0,
+                    indicator = StockIndicator.UP,
+                    details = "Amazon.com, Inc."
                 )
             )
         ).asStateFlow()
@@ -50,7 +58,7 @@ class MainScreenComposeTest {
         coEvery { viewModel.stopWsConnection() } answers { isConnected.value = false }
         coEvery { viewModel.isConnected } returns isConnected
         composeTestRule.setContent {
-            MainScreen(viewModel)
+            MainScreen(rememberNavController(), viewModel)
         }
     }
 
@@ -62,26 +70,27 @@ class MainScreenComposeTest {
 
     @Test
     fun each_row_in_LazyColumn_has_symbol_name_current_price_price_change_indicator() {
-        val firstRow = composeTestRule.onNodeWithTag("LazyColumn").onChildAt(0)
-        firstRow.onChildAt(0).assertTextContains("AAPL")
-        firstRow.onChildAt(1).assertTextContains("$300.00")
-        firstRow.onChildAt(2).assertContentDescriptionContains("Neutral")
+        composeTestRule.onNodeWithTag("LazyColumn", useUnmergedTree = true).onChildAt(0)
+            .onChildAt(0).assertTextContains("AAPL")
+        composeTestRule.onNodeWithTag("LazyColumn", useUnmergedTree = true).onChildAt(0)
+            .onChildAt(1).assertTextContains("$300.00")
+        composeTestRule.onNodeWithTag("LazyColumn", useUnmergedTree = true).onChildAt(0)
+            .onChildAt(2).assertContentDescriptionContains("Neutral")
 
     }
 
     @Test
     fun items_in_LazyColumn_are_sorted_by_price_descending() {
-        composeTestRule.onNodeWithTag("LazyColumn").onChildAt(0).onChildAt(0)
+        composeTestRule.onNodeWithTag("LazyColumn",useUnmergedTree = true).onChildAt(0).onChildAt(0)
             .assertTextContains("AAPL")
-        composeTestRule.onNodeWithTag("LazyColumn").onChildAt(1).onChildAt(0)
+        composeTestRule.onNodeWithTag("LazyColumn",useUnmergedTree = true).onChildAt(1).onChildAt(0)
             .assertTextContains("MSFT")
-        composeTestRule.onNodeWithTag("LazyColumn").onChildAt(2).onChildAt(0)
+        composeTestRule.onNodeWithTag("LazyColumn",useUnmergedTree = true).onChildAt(2).onChildAt(0)
             .assertTextContains("AMZN")
     }
 
     @Test
     fun topbar_has_connection_indicator() {
-
         composeTestRule.onNodeWithTag("ConnectionIndicator").assertExists()
     }
 
@@ -106,11 +115,5 @@ class MainScreenComposeTest {
         coVerify { viewModel.startWsConnection() }
         composeTestRule.onNodeWithTag("ToggleServerButton").performClick()
         coVerify { viewModel.stopWsConnection() }
-    }
-
-    @Test
-    fun tapping_to_a_row_opens_the_symbol_details_page() {
-        composeTestRule.onNodeWithTag("LazyColumn").onChildAt(0).performClick()
-        composeTestRule.onNodeWithTag("SymbolDetailsTitle").assertTextContains("AAPL")
     }
 }
